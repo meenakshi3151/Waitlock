b18970d7fd6352fb47fd35a06f85cdc400e68288
Meenakshi <guptameenakshi957@gmail.com>
2024-06-04 06:06:53 +0530

    added code to index.js and connected to db

--- server/db.js
+++ server/db.js
@@ -2,12 +2,12 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI, {
+        const conn = await mongoose.connect(process.env.MONGO_URL, {
            
         });
-        console.log(`MongoDB Connected: ${conn.connection.host}` .cyan.underline);
+        console.log(`MongoDB Connected: ${conn.connection.host}` );
     } catch (error) {
-        console.error(`Error: ${error.message}` .red.bold);
+        console.error(`Error: ${error.message}` );
         process.exit(1); 
     }
 };
