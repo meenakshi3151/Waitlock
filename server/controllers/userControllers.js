1cc9ccb9d513e78412976c778a6fc5ca2b8427b8
Meenakshi <guptameenakshi957@gmail.com>
2024-06-04 06:21:37 +0530

    added controller for user

---
+++ server/controllers/userControllers.js
@@ -0,0 +1,50 @@
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModels");
+
+//register the user
+const registerUser = async (req, res) => {
+    const { name, email, password, year, branch,
+        course, phoneNo, registrationNo
+    } = req.body;
+
+    if (!name || !email || !password || !year || !branch || !course || !phoneNo || !registrationNo) {
+        res.status(400);
+        throw new Error("Please Enter all the Feilds");
+    }
+
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+        res.status(400);
+        throw new Error("User already exists");
+    }
+
+    const user = await User.create({
+        name,
+        email,
+        password,
+        year,
+        branch,
+        course,
+        phoneNo,
+        registrationNo
+    });
+
+    if (user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            year: user.year,
+            branch: user.branch,
+            course: user.course,
+            phoneNo: user.phoneNo,
+            registrationNo: user.registrationNo,
+            token: generateToken(user._id),
+        });
+    } else {
+        res.status(400);
+        throw new Error("User not found");
+    }
+};
+module.exports = { registerUser }
