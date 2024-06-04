1b6f01e6c6211ff460ebdf532452adf53689a2b7
Meenakshi <guptameenakshi957@gmail.com>
2024-06-04 11:05:27 +0530

    added email routes for sending mails

---
+++ server/routes/emailRoutes.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const router = express.Router();
+
+const { sendEmail } = require("../controllers/emailControllers");
+router.post("/sendEmail", sendEmail);
+module.exports = router;
