5828a0bda969df8448ae89d202f76aa731da46c1
Ishsinghal <ishsinghal0707@gmail.com>
2024-06-05 11:16:26 +0530

    Added signup Functionality

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,17 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Footer from './components/Footer';
-import NavBar from './components/Navbar';
+
+// import NavBar from './components/Navbar';
 import HomePage from './pages/HomePage';
 function App() {
   return (
  <>
  <HomePage/>
+
+  {/* <HomePage></HomePage> */}
+  <SignUpForm></SignUpForm>
+
  </>
 
 
