0c22e9ef4350ff9bf5881802a36cd7cd44325d1c
Ishsinghal <ishsinghal0707@gmail.com>
2024-06-05 12:20:18 +0530

    Routes given to all page

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Footer from './components/Footer';
 import SignUpForm from './components/Signup';
 // import NavBar from './components/Navbar';
 import Login from './components/Login';
 import HomePage from './pages/HomePage';
+import {Route,Routes} from 'react-router-dom';
 function App() {
   return (
  <>
  {/* <HomePage/> */}
 
   {/* <HomePage></HomePage> */}
-  <Login></Login>
-  <SignUpForm></SignUpForm>
-
+  <Routes>
+    <Route path="/" element={<HomePage/>}></Route>
+    <Route path="/login" element={<Login/>}></Route>
+    <Route path="/signup" element={<SignUpForm/>}></Route>
+  
+  </Routes>
  </>
 
 
