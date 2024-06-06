dd70130fcf8f5fc12149d4c197547f1cbc61bca0
Meenakshi <guptameenakshi957@gmail.com>
2024-06-06 22:50:11 +0530

    added toasts messages for the user

--- client/src/components/StudentDashboard.js
+++ client/src/components/StudentDashboard.js
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
+import {useToast} from 'react';
 
 function StudentDashboard() {
  
   const [GoingOut, setGoingOut] = useState(false);
 
- 
+  const toast=useToast();
 
   const handleButton = async () => {
+   
     const response = await fetch('http://localhost:5000/goingOut', {
       method: 'PUT',
       headers: {
@@ -17,8 +19,24 @@ function StudentDashboard() {
 
     if (response.ok) {
       console.log('Success:', response);
+
       setGoingOut(!userInfo.goingOut);
+      toast({
+        title: "Thanks for doing the entry. Have a good Day!!",
+        status: "success",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      })
+      return;
     } else {
+        toast({
+            title: "Error occured",
+            status: "warning",
+            duration: 5000,
+            isClosable: true,
+            position: "bottom",
+          })
       console.log('Error:', response);
     }
   };
